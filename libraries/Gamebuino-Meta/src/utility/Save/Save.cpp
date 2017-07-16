30ba5513f56302342461ed4765bdc6e714e073f0
Sorunome <mail@sorunome.de>
2017-07-16 19:25:39 +0200

    fix freezing when opening savefile

--- libraries/Gamebuino-Meta/src/utility/Save/Save.cpp
+++ libraries/Gamebuino-Meta/src/utility/Save/Save.cpp
@@ -113,7 +113,7 @@ void Save::openFile() {
 	// first check that the checkbytes match!
 	// we use the payload_size for this because that is just yet another free 4-byte buffer
 	f.read(&payload_size, 4);
-	if ((uint32_t)payload_size != *((uint32_t*)checkbytes)) {
+	if (memcmp(&payload_size, checkbytes, 4) != 0) {
 		error("Invalid save file");
 	}
 	
