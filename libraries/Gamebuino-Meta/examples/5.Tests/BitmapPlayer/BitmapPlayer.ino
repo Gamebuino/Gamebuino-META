bbbbce761e3aa3216ea863b4d8176bbf9bdf9726
Aurélien Rodot <rodot@gamebuino.com>
2017-07-17 19:47:43 +0200

    Add bitmap player

--- libraries/Gamebuino-Meta/examples/5.Tests/BitmapPlayer/BitmapPlayer.ino
+++ libraries/Gamebuino-Meta/examples/5.Tests/BitmapPlayer/BitmapPlayer.ino
@@ -6,6 +6,8 @@ int currentFile = 0;
 File root;
 File entry;
 char path[50];
+int popupTimer = 0;
+int nextTimer = 0;
 
 Image img;
 void setup() {
@@ -33,21 +35,42 @@ boolean loadNextBitmap() {
     entry.close();
     gb.display.println(path);
   } while (!(strstr(path, ".BMP") || strstr(path, ".bmp")));
-  gb.tft.print("LOADING IMAGE");
-  img.init(path, 0);
+  gb.tft.cursorX = 0;
+  gb.tft.cursorY = 0;
+  gb.tft.setFontSize(2);
+  gb.tft.setColor(WHITE, BLACK);
+  gb.tft.print(path);
+  gb.tft.setFontSize(1);
+  img.init(80, 64, path);
+  popupTimer = 20;
+  nextTimer = 50;
   return true;
 }
 
 void loop() {
   if (gb.update()) {
+    //if multiple frames
+    if (img.frames > 1) { 
+      //load after the last one
+      if (img.frame == (img.frames - 1)) {
+        loadNextBitmap();
+      }
+      //if single frame, load after a while
+    } else {
+      nextTimer--;
+      if (nextTimer <= 0) {
+      loadNextBitmap();
+      }
+    }
 
-    if ((gb.frameCount % 50) == 0) {
+    if (gb.buttons.repeat(BUTTON_A, 10)) {
       loadNextBitmap();
     }
     gb.display.drawImage(0, 0, img);
-    if ((gb.frameCount % 50) <= 20) {
+    if (popupTimer > 0) {
+      popupTimer--;
       //show file name
-      gb.display.setColor(WHITE,BLACK);
+      gb.display.setColor(WHITE, BLACK);
       gb.display.cursorX = 0;
       gb.display.cursorY = 0;
       gb.display.println(path);
