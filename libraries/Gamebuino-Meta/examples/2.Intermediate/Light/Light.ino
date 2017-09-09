75faedb99d9addefea4c6a4e9622e901180c32d5
Sorunome <mail@sorunome.de>
2017-09-09 19:54:42 +0200

    added light example

---
+++ libraries/Gamebuino-Meta/examples/2.Intermediate/Light/Light.ino
@@ -0,0 +1,41 @@
+// import the Gamebuino library
+#include <Gamebuino-Meta.h>
+
+// gamebuino setup
+void setup() {
+  gb.begin();
+}
+
+// gb.light is a 2x4 pixel image to which we can draw to
+// drawing stuff to it will automatically update the lights in the background
+
+bool lightsOn = false;
+bool redLight = false;
+
+void loop() {
+  if (gb.update()) {
+    if (lightsOn) {
+      // if we want the lights to be on, let's turn all lights white!
+      gb.light.fillScreen(WHITE);
+    }
+    
+    if (redLight) {
+      // if we want the red light, let's set the lower-left light red
+      // if lightsOn is true we will just over-write the lower-left light, and thus the lower-left light **WILL** be red for sure!
+      gb.light.drawPixel(0, 3, RED);
+    }
+    
+    if (gb.buttons.pressed(BUTTON_A)) {
+      // toggle lightsOn
+      lightsOn = !lightsOn;
+    }
+    
+    if (gb.buttons.pressed(BUTTON_B)) {
+      // toggle redLight
+      redLight = !redLight;
+    }
+    
+    // now let's just display our controls
+    gb.display.println("A: lights on\nB: red light");
+  }
+}
