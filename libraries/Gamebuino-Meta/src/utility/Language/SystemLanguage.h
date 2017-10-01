56d771a5cfabc84f93bac31799ca566d8362bebf
Drakker <dpelletier@gmail.com>
2017-09-30 21:00:48 -0400

    Added french translation / Ajouté la traduction française

--- libraries/Gamebuino-Meta/src/utility/Language/SystemLanguage.h
+++ libraries/Gamebuino-Meta/src/utility/Language/SystemLanguage.h
@@ -2,7 +2,7 @@
 
 namespace Gamebuino_Meta {
 
-#define NUMBER_SYSTEM_LANGUAGES 2
+#define NUMBER_SYSTEM_LANGUAGES 3
 
 
 //////////
@@ -12,61 +12,73 @@ namespace Gamebuino_Meta {
 const MultiLang lang_homeMenu_exit[] = {
 	{LangCode::en, "EXIT      "},
 	{LangCode::de, "VERLASSEN "},
+	{LangCode::fr, "QUITTER   "},
 };
 
 const MultiLang lang_homeMenu_volume[] = {
 	{LangCode::en, "VOLUME    "},
 	{LangCode::de, "LAUTSTÄRKE"},
+	{LangCode::fr, "VOLUME    "},
 };
 
 const MultiLang lang_homeMenu_save_image[] = {
 	{LangCode::en, "SAVE IMAGE"},
 	{LangCode::de, "BILD SPCHR"},
+	{LangCode::fr, "ENRG IMAGE"},
 };
 
 const MultiLang lang_homeMenu_save_video[] = {
 	{LangCode::en, "SAVE VIDEO"},
 	{LangCode::de, "VID SPCHRN"},
+	{LangCode::fr, "ENRG VIDEO"},
 };
 
 const MultiLang lang_homeMenu_light[] = {
 	{LangCode::en, "LIGHT     "},
 	{LangCode::de, "LICHT     "},
+	{LangCode::fr, "LUMINOSITÉ"},
 };
 
 const MultiLang lang_homeMenu_SELECT[] = {
 	{LangCode::en, "SELECT  "},
 	{LangCode::de, "WÄHLEN  "},
+	{LangCode::fr, "CHOISIR "},
 };
 
 const MultiLang lang_homeMenu_RESUME[] = {
 	{LangCode::en, "RESUME  "},
 	{LangCode::de, "ZURÜCK  "},
+	{LangCode::fr, "RETOUR  "},
 };
 
 const MultiLang lang_homeMenu_SAVING[] = {
 	{LangCode::en, "SAVING... "},
 	{LangCode::de, "SICHERE..."},
+	{LangCode::fr, "ENREGISTRE"},
 };
 
 const MultiLang lang_homeMenu_SAVED[] = {
 	{LangCode::en, "SAVED!    "},
 	{LangCode::de, "GESICHERT!"},
+	{LangCode::fr, "ENREGISTRÉ"},
 };
 
 const MultiLang lang_homeMenu_ERROR[] = {
 	{LangCode::en, "ERROR     "},
 	{LangCode::de, "FEHLER    "},
+	{LangCode::fr, "ERREUR    "},
 };
 
 const MultiLang lang_homeMenu_READY[] = {
 	{LangCode::en, "READY?    "},
 	{LangCode::de, "BEREIT?   "},
+	{LangCode::fr, "PRÊT?     "},
 };
 
 const MultiLang lang_homeMenu_GO[] = {
 	{LangCode::en, "GO!       "},
 	{LangCode::de, "LOS!      "},
+	{LangCode::fr, "PARTEZ!   "},
 };
 
 
@@ -77,16 +89,19 @@ const MultiLang lang_homeMenu_GO[] = {
 const MultiLang lang_keyboard_type[] = {
 	{LangCode::en, "type"},
 	{LangCode::de, "tippen"},
+	{LangCode::fr, "écrire"},
 };
 
 const MultiLang lang_keyboard_back[] = {
 	{LangCode::en, "back"},
 	{LangCode::de, "zurück"},
+	{LangCode::fr, "retour"},
 };
 
 const MultiLang lang_keyboard_save[] = {
 	{LangCode::en, "save"},
 	{LangCode::de, "fertig"},
+	{LangCode::fr, "enregistrer"},
 };
 
 
@@ -97,6 +112,7 @@ const MultiLang lang_keyboard_save[] = {
 const MultiLang lang_titlescreen_a_start[] = {
 	{LangCode::en, "\x15 to start"},
 	{LangCode::de, "\x15 zum starten"},
+	{LangCode::fr, "\x15 pour commencer"},
 };
 
 }; // namespace Gamebuino_Meta
