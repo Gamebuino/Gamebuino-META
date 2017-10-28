4a3779ec4da3b52bcb8ccc7cb4cb131e00c225fc
Aurélien Rodot <rodot@gamebuino.com>
2017-10-28 15:46:54 +0200

    Add multiplayer to Pong2

--- libraries/Gamebuino-Meta/examples/2.Intermediate/Pong2/Pong2.ino
+++ libraries/Gamebuino-Meta/examples/2.Intermediate/Pong2/Pong2.ino
@@ -10,6 +10,8 @@ int player_x = 4;
 int player_y = (gb.display.height() - player_h) / 2;
 int player_vymax = 4;
 int player_vy = 0;
+int player_timeoutmax = 80;
+int player_timeout = player_timeoutmax;
 
 //oponent variables
 int oponent_score = 0;
@@ -19,6 +21,8 @@ int oponent_x = gb.display.width() - oponent_w - 4;
 int oponent_y = (gb.display.height() - oponent_h) / 2;
 int oponent_vymax = 4;
 int oponent_vy = 0;
+int oponent_timeoutmax = 80;
+int oponent_timeout = oponent_timeoutmax;
 
 //ball variables
 int ball_size = 3;
@@ -45,7 +49,7 @@ void loop() {
     // clear the previous screen
     gb.display.clear();
     gb.lights.clear();
-    
+
 
     //pause the game if C is pressed
     if (gb.buttons.pressed(BUTTON_C)) {
@@ -56,17 +60,65 @@ void loop() {
     }
     //move the player
     if (gb.buttons.repeat(BUTTON_UP, 1)) {
+      player_timeout = player_timeoutmax;
       player_vy -= 1;
-      player_vy = constrain(player_vy, -player_vymax, player_vymax);
     } else {
       if (gb.buttons.repeat(BUTTON_DOWN, 1)) {
+        player_timeout = player_timeoutmax;
         player_vy += 1;
-        player_vy = constrain(player_vy, (-player_vymax), player_vymax);
       } else {
-        player_vy = player_vy / 2;
+        if (player_timeout > 0) {
+          player_vy = player_vy / 2;
+        }
       }
     }
-    player_y = constrain(player_y + player_vy, 0, gb.display.height() - player_h);
+    //computer control after timeout
+    if (player_timeout > 0) {
+      player_timeout--;
+    } else {
+      if ((player_y + (player_h / 2)) < (ball_y + (ball_size / 2))) { //if the ball is below the oponent
+        player_vy = player_vy + 1; //move down
+      }
+      else {
+        player_vy = player_vy - 1; //move up
+      }
+    }
+    player_vy = constrain(player_vy, (-player_vymax), player_vymax);
+    player_y = player_y + player_vy;
+    player_y = constrain(player_y, (-player_h), gb.display.height());
+
+
+    //move the oponent
+    //human control
+    if (gb.buttons.repeat(BUTTON_B, 1)) {
+      oponent_timeout = oponent_timeoutmax;
+      oponent_vy -= 1;
+      oponent_vy = constrain(oponent_vy, (-oponent_vymax), oponent_vymax);
+    } else {
+      if (gb.buttons.repeat(BUTTON_A, 1)) {
+        oponent_timeout = oponent_timeoutmax;
+        oponent_vy += 1;
+        oponent_vy = constrain(oponent_vy, (-oponent_vymax), oponent_vymax);
+      } else {
+        if (oponent_timeout > 0) {
+          oponent_vy = oponent_vy / 2;
+        }
+      }
+    }
+    //computer control after timeout
+    if (oponent_timeout > 0) {
+      oponent_timeout--;
+    } else {
+      if ((oponent_y + (oponent_h / 2)) < (ball_y + (ball_size / 2))) { //if the ball is below the oponent
+        oponent_vy = oponent_vy + 1; //move down
+      }
+      else {
+        oponent_vy = oponent_vy - 1; //move up
+      }
+    }
+    oponent_vy = constrain(oponent_vy, (-oponent_vymax), oponent_vymax);
+    oponent_y = oponent_y + oponent_vy;
+    oponent_y = constrain(oponent_y, (-oponent_h), gb.display.height()); //don't go out of the screen
 
     //move the ball
     ball_x = ball_x + ball_vx;
@@ -109,10 +161,11 @@ void loop() {
       gb.lights.setColor(RED);
       gb.lights.drawPixel(0, map(ball_y, 0, gb.display.height(), 0, 4));
       player_y = (gb.display.height() / 2) - (player_h / 2);
+      oponent_y = player_y;
       ball_x = oponent_x - 4;
       ball_vx = -abs(ball_vx);
       ball_vy = 0;
-      ball_y = gb.display.height() / 2;
+      ball_y = random(0, gb.display.height() - ball_size);
     }
     //collision with the right side
     if ((ball_x + ball_size) > gb.display.width()) {
@@ -121,10 +174,11 @@ void loop() {
       gb.lights.setColor(RED);
       gb.lights.drawPixel(1, map(ball_y, 0, gb.display.height(), 0, 4));
       player_y = (gb.display.height() / 2) - (player_h / 2);
-      ball_x = oponent_x - 4;
-      ball_vx = -abs(ball_vx);
+      oponent_y = player_y;
+      ball_x = player_x + player_w + 5;
+      ball_vx = abs(ball_vx);
       ball_vy = 0;
-      ball_y = gb.display.height() / 2;
+      ball_y = random(0, gb.display.height() - ball_size);
 
     }
     //reset score when 10 is reached
@@ -133,34 +187,42 @@ void loop() {
       oponent_score = 0;
     }
 
-    //move the oponent
-    if ((oponent_y + (oponent_h / 2)) < (ball_y + (ball_size / 2))) { //if the ball is below the oponent
-      oponent_vy = oponent_vy + 1; //move down
-    }
-    else {
-      oponent_vy = oponent_vy - 1; //move up
-    }
-    oponent_vy = constrain(oponent_vy, (-oponent_vymax), oponent_vymax);
-    oponent_y = oponent_y + oponent_vy;
-    //oponent_y = constrain(oponent_y + oponent_vy, 0, gb.display.height() - oponent_h); //don't go out of the screen
-
-
+    gb.display.setColor(WHITE);
     //draw the score
     //gb.display.fontSize = 2;
     gb.display.setCursor(15, 16);
     gb.display.print(player_score);
 
-    gb.display.setCursor(57, 16);
+    gb.display.setCursor(gb.display.width() - 3 - 15, 16);
     gb.display.print(oponent_score);
-    //draw the ball
-    gb.display.fillRect(ball_x, ball_y, ball_size, ball_size);
-    //draw the player
-    gb.display.fillRect(player_x, player_y, player_w, player_h);
-    //draw the oponent
-    gb.display.fillRect(oponent_x, oponent_y, oponent_w, oponent_h);
+
     //draw the middle line
     for (int i = 0; i < gb.display.height(); i += 4) {
       gb.display.drawRect(gb.display.width() / 2, i, 1, 2);
+    }
+    //draw the ball
+    gb.display.fillRect(ball_x, ball_y, ball_size, ball_size);
+    //draw the player
+    if (player_timeout == 0) {
+      gb.display.setColor(GRAY);
+    } else {
+      gb.display.setColor(WHITE);
+    }
+    gb.display.fillRect(player_x, player_y, player_w, player_h);
+    //draw the oponent
+    if (oponent_timeout == 0) {
+      gb.display.setColor(GRAY);
+    } else {
+      gb.display.setColor(WHITE);
+    }
+    gb.display.fillRect(oponent_x, oponent_y, oponent_w, oponent_h);
+    //TRY ME! message
+    if ((player_timeout == 0) && (oponent_timeout == 0)) {
+      gb.display.setCursor(27, 16);
+      gb.display.setColor(WHITE, BLACK);
+      if ((gb.frameCount % 20) > 10) {
+        gb.display.print("TRY ME!");
+      }
     }
   }
 }
