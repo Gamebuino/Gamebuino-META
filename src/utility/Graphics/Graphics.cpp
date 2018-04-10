af067a50149e17dc9fb5a2d9276b822751047547
Sorunome <mail@sorunome.de>
2018-04-10 10:51:04 +0200

    fixed negative vertical lines for real

--- src/utility/Graphics/Graphics.cpp
+++ src/utility/Graphics/Graphics.cpp
@@ -334,11 +334,19 @@ void Graphics::drawRect(int16_t x, int16_t y, int16_t w, int16_t h) {
 
 void Graphics::drawFastVLine(int16_t x, int16_t y, int16_t h) {
 	// Update in subclasses if desired!
+	if (h < 0) {
+		y += h + 1;
+		h = abs(h);
+	}
 	drawLine(x, y, x, y+h-1);
 }
 
 void Graphics::drawFastHLine(int16_t x, int16_t y, int16_t w) {
 	// Update in subclasses if desired!
+	if (w < 0) {
+		x += w + 1;
+		w = abs(w);
+	}
 	drawLine(x, y, x+w-1, y);
 }
 
